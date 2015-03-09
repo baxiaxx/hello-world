0bbb0e03465030339187677d3da55082f25e2abb
BaxiaXX <kumahikarihui@BravoFuture.local>
2015-03-09 22:28:20 +0800

    first add NSDictionary *a = @{@"a": @"b"};

--- helloworld/AppDelegate.m
+++ helloworld/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSDictionary *a = @{@"a": @"b"};
     return YES;
 }
 							
